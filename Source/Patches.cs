awgv/ColonialShuttle

commit 40a95ef33c77ab0147adadef58edc1ae0d9a8154

fix: `fuelConsumptionRatePerKilogramAfterUpgrades`
Some upgrade combinations were evaluating to the default `fuelConsumptionRatePerKilogram`. Also, change upgrade descriptions for clarity.

About/About.xml
@@ -3,7 +3,7 @@
   <packageId>awgv.ColonialShuttle</packageId>
   <name>Colonial Shuttle</name>
   <author>J</author>
-  <modVersion>1.1.0</modVersion>
+  <modVersion>1.1.1</modVersion>
   <supportedVersions>
     <li>1.5</li>
   </supportedVersions>

Source/Patches.cs
@@ -19,22 +19,20 @@ namespace ColonialShuttle
         internal const float fuelConsumptionRatePerKilogram = 4.54f * fuelConsumptionRateMultiplier;
 
         /// <summary>
-        /// Dictionary keys are keys of upgrade nodes.
+        /// Dictionary keys are the keys of upgrade nodes. These are essentially
+        /// percentages of `fuelConsumptionRatePerKilogram`, so make sure that
+        /// no combination of upgrades allow `fuelConsumptionRatePerKilogram` to
+        /// become <= zero, because it’s going to be reset to the default value.
         /// </summary>
         internal static Dictionary<string, float> fuelConsumptionRateMultipliersForUpgrades =
             new Dictionary<string, float>()
             {
-                { "CargoRacksB", 1f },
-                { "CargoRacksA", 2f },
-                { "ThrustersC", -0.5f },
-                { "ThrustersB", -1f },
-                { "ThrustersA", -2f },
+                { "CargoRacksB", 0.25f },
+                { "CargoRacksA", 0.5f },
+                { "ThrustersC", -0.125f },
+                { "ThrustersB", -0.25f },
+                { "ThrustersA", -0.5f },
             };
-
-        /// <summary>
-        /// Transport pods spend 2.25 chemfuel per tile or 675f.
-        /// </summary>
-        internal const float minimumFuelConsumptionRate = 675f * fuelConsumptionRateMultiplier;
     }
 
     [StaticConstructorOnStartup]
@@ -108,6 +106,12 @@ namespace ColonialShuttle
                 }
             }
 
+            if (fuelConsumptionRatePerKilogramAfterUpgrades <= 0)
+            {
+                fuelConsumptionRatePerKilogramAfterUpgrades =
+                    Defaults.fuelConsumptionRatePerKilogram;
+            }
+
             float totalWeightOfCargoInKilograms = MassUtility.InventoryMass(__instance.Vehicle);
 
             foreach (var pawn in __instance.Vehicle.AllPawnsAboard)
@@ -117,9 +121,8 @@ namespace ColonialShuttle
 
             float fuelConsumptionRate =
                 totalWeightOfCargoInKilograms * fuelConsumptionRatePerKilogramAfterUpgrades;
-            return fuelConsumptionRate <= Defaults.minimumFuelConsumptionRate
-                ? Defaults.minimumFuelConsumptionRate
-                : fuelConsumptionRate;
+
+            return fuelConsumptionRate;
         }
     }
 }

1.5/Defs/Shuttle_UpgradeTree.xml
@@ -233,7 +233,7 @@
       <li>
         <key>CargoRacksB</key>
         <label>Cargo racks (B)</label>
-        <description>\nReplaces A. Half of the resources will be refunded.\n\n+250 cargo capacity\n+100% chemfuel consumption per kg carried</description>
+        <description>\nReplaces A. Half of the resources will be refunded.\n\n+250 cargo capacity\n+25% chemfuel consumption per kg carried</description>
         <icon>UI/ColonialShuttle_Cargo_Racks_B</icon>
         <work>1000</work>
         <gridCoordinate>(12,7)</gridCoordinate>
@@ -261,7 +261,7 @@
       <li>
         <key>CargoRacksA</key>
         <label>Cargo racks (A)</label>
-        <description>\nReplaces B. Half of the resources will be refunded.\n\n+500 cargo capacity\n+200% chemfuel consumption per kg carried</description>
+        <description>\nReplaces B. Half of the resources will be refunded.\n\n+500 cargo capacity\n+50% chemfuel consumption per kg carried</description>
         <icon>UI/ColonialShuttle_Cargo_Racks_A</icon>
         <work>2000</work>
         <gridCoordinate>(17,7)</gridCoordinate>
@@ -289,7 +289,7 @@
       <li>
         <key>ThrustersC</key>
         <label>Thrusters (C)</label>
-        <description>\nReplaces B/A. Half of the resources will be refunded.\n\n+150 cargo capacity\n-50% chemfuel consumption per kg carried</description>
+        <description>\nReplaces B/A. Half of the resources will be refunded.\n\n+150 cargo capacity\n-12.5% chemfuel consumption per kg carried</description>
         <icon>UI/ColonialShuttle_Thrusters_C</icon>
         <work>10000</work>
         <gridCoordinate>(7,12)</gridCoordinate>
@@ -321,7 +321,7 @@
       <li>
         <key>ThrustersB</key>
         <label>Thrusters (B)</label>
-        <description>\nReplaces C/A. Half of the resources will be refunded.\n\n+260 cargo capacity\n-100% chemfuel consumption per kg carried</description>
+        <description>\nReplaces C/A. Half of the resources will be refunded.\n\n+260 cargo capacity\n-25% chemfuel consumption per kg carried</description>
         <icon>UI/ColonialShuttle_Thrusters_B</icon>
         <work>16000</work>
         <gridCoordinate>(12,12)</gridCoordinate>
@@ -356,7 +356,7 @@
       <li>
         <key>ThrustersA</key>
         <label>Thrusters (A)</label>
-        <description>\nReplaces C/B. Half of the resources will be refunded.\n\n+470 cargo capacity\n-200% chemfuel consumption per kg carried</description>
+        <description>\nReplaces C/B. Half of the resources will be refunded.\n\n+470 cargo capacity\n-50% chemfuel consumption per kg carried</description>
         <icon>UI/ColonialShuttle_Thrusters_A</icon>
         <work>24000</work>
         <gridCoordinate>(17,12)</gridCoordinate>

commit 845365baba533c1802529aebd62ddfc85b107a26

fix: fuel consumption rate being 0 when flying with 0 kg of cargo
It’s a regresssion introduced in 40a95ef33c77ab0147adadef58edc1ae0d9a8154.

About/About.xml
@@ -3,7 +3,7 @@
   <packageId>awgv.ColonialShuttle</packageId>
   <name>Colonial Shuttle</name>
   <author>J</author>
-  <modVersion>1.1.3</modVersion>
+  <modVersion>1.1.4</modVersion>
   <supportedVersions>
     <li>1.5</li>
   </supportedVersions>

Source/Patches.cs
@@ -38,6 +38,11 @@ namespace ColonialShuttle
                 { "ThrustersB", -0.25f },
                 { "ThrustersA", -0.5f },
             };
+
+        /// <summary>
+        /// Transport pods spend 2.25 chemfuel per tile or 675f. Used when shuttles carry no cargo.
+        /// </summary>
+        internal const float minimumFuelConsumptionRate = 338f;
     }
 
     [StaticConstructorOnStartup]
@@ -125,7 +130,9 @@ namespace ColonialShuttle
             }
 
             float fuelConsumptionRate =
-                totalWeightOfCargoInKilograms * fuelConsumptionRatePerKilogramAfterUpgrades;
+                totalWeightOfCargoInKilograms == 0
+                    ? Defaults.minimumFuelConsumptionRate
+                    : totalWeightOfCargoInKilograms * fuelConsumptionRatePerKilogramAfterUpgrades;
 
             return fuelConsumptionRate;
         }